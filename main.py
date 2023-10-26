23f9772fe5d6902f92285f3c4efaa53400b28302
nguyenquocthao <nguyenquocthao00@gmail.com>
2023-10-26 09:56:56 +0700

    update

--- main.py
+++ main.py
@@ -1,29 +1,24 @@
-import os
-from langchain.agents import load_tools, initialize_agent, AgentType
-from langchain.llms import OpenAI, AzureOpenAI
-
+from langchain.agents import load_tools, initialize_agent
+from langchain.agents import AgentType
+from langchain.chat_models import ChatOpenAI
 from dotenv import load_dotenv
+import langchain
 
 load_dotenv()
+langchain.debug=True
 
-# print(os.environ["OPENAI_API_KEY"], os.environ["SERPAPI_API_KEY"])
-# os.environ["OPENAI_API_KEY"] = 
-if os.environ.get("OPENAI_API_TYPE", "") == "azure":
-    print(12, "fdnfdn")
-    llm = AzureOpenAI(deployment_name="gpt-35-turbo-version-0301", model_name="gpt-35-turbo (version 0301)", temperature=0, max_tokens=512)
-else:
-    llm = OpenAI(temperature=0)
-tools = load_tools(["serpapi", "llm-math"], llm=llm)
-agent = initialize_agent(
-    tools, llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
-)
+llm = ChatOpenAI(temperature=0)
+tools = load_tools(["llm-math","wikipedia"], llm=llm)
 
-# agent.run(
-#     "What was the high temperature in SF yesterday in Fahrenheit? What is that number raised to the .023 power?"
-# )
-
-agent.run(
-    "Langchain difference between OpenAI and AzureOpenAI"
-)
-
-# print(llm("Do you know langchain? Do you have the link to langchain?"))
+agent= initialize_agent(
+    tools, 
+    llm, 
+    agent=AgentType.CHAT_ZERO_SHOT_REACT_DESCRIPTION,
+    handle_parsing_errors=True,
+    )
+print(agent("What is the 25% of 300?"))
+question = "Tom M. Mitchell is an American computer scientist \
+and the Founders University Professor at Carnegie Mellon University (CMU)\
+what book did he write?"
+result = agent(question) 
+print(result)
